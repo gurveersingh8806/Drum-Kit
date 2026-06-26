fe2cdfbc33f9d12e3c309c7ed89ac49d60da3821
GURVEER SINGH <gurveersingh8806@gmail.com>
2026-06-26 22:22:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+var cnt = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < cnt; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+
+    var ele = this.innerHTML;
+
+    makeSound(ele);
+
+    addAnimation(ele);
+
+    });
+}
+
+
+document.addEventListener("keydown",function(event){
+
+    makeSound(event.key);
+
+    addAnimation(event.key);
+});
+
+function makeSound(key){
+
+    switch (key) {
+        case 'w':
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case 'a':
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case 's':
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case 'd':
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case 'j':
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        
+        case 'k':
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case 'l':
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+    
+        default:
+            break;
+    }
+
+}
+
+function addAnimation(event){
+
+    var box = document.querySelector("." + event);
+
+    box.classList.add("pressed");
+
+    setTimeout(function(){
+        
+        box.classList.remove("pressed");
+    }, 100);
+}
